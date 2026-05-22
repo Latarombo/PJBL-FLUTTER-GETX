fc8d4712967ca11e29e6bcc96292d2fdc361952d
Latarombo <zidane544@gmail.com>
2026-05-22 23:52:54 +0700

    Add Firebase integration and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:santarana/firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
 import 'shared/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Hapus setelah berhasil test
+  print('Firebase connected: ${Firebase.app().name}');
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
